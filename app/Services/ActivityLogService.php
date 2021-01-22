54671f6c92cea39b03653b3ccb510b169f6e54cb
darkmat13r <darkmat13r@gmail.com>
2021-01-22 21:07:34 +0530

    fix step log

--- app/Services/ActivityLogService.php
+++ app/Services/ActivityLogService.php
@@ -26,21 +26,24 @@ class ActivityLogService
         if (!in_array($activity, $this->activities)) {
             throw  new GeneralException(__('activity.errors.invalid_activity', $activity));
         }
+        $dateObj = Carbon::now();
         try {
-            $dateObj = Carbon::createFromFormat('Y-m-d H:i:s', $date);
+            if($date){
+                $dateObj = Carbon::createFromFormat('Y-m-d H:i:s', $date);
+            }
         } catch (\Exception $exception) {
             throw new GeneralException(__('activity.errors.invalid_date'));
         }
 
         $lastLog = ActivityLog::where('user_id', $userId)->whereDate('created_at', $dateObj->format('Y-m-d'))->orderBy('id', 'DESC')->first();
         $offset = 0;
-        /*if ($lastLog) {
+        if ($lastLog) {
             if ($dateObj->shiftTimezone('UTC')->lessThanOrEqualTo($lastLog->created_at)) {
                 throw new GeneralException(__('activity.errors.invalid_data'));
             }
             $lastLog->value  = $value;
             $lastLog->save();
-            $lastSum  =  $this->activityLogRepo->getSumByDate($userId, $dateObj->format('Y-m-d'));
+           /* $lastSum  =  $this->activityLogRepo->getSumByDate($userId, $dateObj->format('Y-m-d'));
             $oldOffset  =  $this->activityLogRepo->getOffsetByDate($userId, $dateObj->format('Y-m-d'));
             $newValue = $value - $lastSum - $oldOffset;
             if ($newValue < 0) {
@@ -48,11 +51,11 @@ class ActivityLogService
             }
             if ($newValue === 0) {
                 return $lastLog;
-            }
+            }*/
         }else{
             $offset = $value;
             $newValue = 0;
-        }*/
+        }
         return $this->activityLogRepo->create($userId, $activity, $value, $offset, $date);
     }
 }
