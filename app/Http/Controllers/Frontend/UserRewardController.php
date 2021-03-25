81a229f494c29ffb181270ab5803d34f2f0b1da3
darkmat13r <35571567+darkmat13r@users.noreply.github.com>
2021-03-25 15:57:57 +0530

    document upload

--- app/Http/Controllers/Frontend/UserRewardController.php
+++ app/Http/Controllers/Frontend/UserRewardController.php
@@ -22,7 +22,23 @@ class UserRewardController extends Controller
         $user = Auth::user();
         $rewards = $this->rewardHistoryRepo->getRewards($user);
         return JsonResponse::success([
-            'history' => $rewards,
+            'rewards' => $rewards,
+            'payouts' => [[
+                'bank'=> [
+                    'bank_name' => '',
+                    'account_number' => '',
+                    'account_type' => '',
+                    'account_name' => '',
+                ],
+                'amount' =>   12.10,
+                'created_at' =>''
+            ]],
+            'payout' =>  12.10,
+            'total_certified' =>  12.10,
+            'active_growth' =>  12.10,
+            'active_achieved' => 12.10,
+            'passive_growth' =>  12.10,
+            'passive_achieved' => 12.10,
             'balance' => $user->wallet_amount
         ]);
 
